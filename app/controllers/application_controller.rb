6b25015ca1987ce2ccf913482642038b6569ee37
Samuel Johns <samuel.johns@moat.com>
2016-10-18 18:15:16 -0400

    Uses rack-cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
 
-  after_filter :allow_cors
+#  after_filter :allow_cors
 
   def allow_cors
     headers['Access-Control-Allow-Origin'] = '*'
